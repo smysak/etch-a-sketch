3b108d1f47d1ebf9562dd0a67a1c956e0b8b7e49
smysak <98991422+smysak@users.noreply.github.com>
2026-04-29 09:01:25 +0800

    set up a hover effect on mouseover/mouseleave gridBoxes will now light up on mouseover

--- script.js
+++ script.js
@@ -2,8 +2,22 @@ const container = document.getElementById("container");
 
 for (let i = 0; i < 16; i++) {
     const gridDiv = document.createElement('div');
-    gridDiv.id = `Box ${i + 1}`;
+    gridDiv.id = `box ${i + 1}`;
     gridDiv.className = "gridBox";
-    gridDiv.textContent = `Box ${i + 1}`;
     container.appendChild(gridDiv);
 }
+
+const boxes = document.querySelectorAll(".gridBox");
+
+boxes.forEach(box => {
+    box.addEventListener('mouseenter', () => {
+        box.style.backgroundColor = 'yellow';
+    });
+});
+
+boxes.forEach(box => {
+    box.addEventListener('mouseleave', () => {
+        box.style.backgroundColor = 'white';
+    });
+});
+
